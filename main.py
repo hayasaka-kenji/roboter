664bd163ff0afaf8408ead19ff1b5ee972a17a35
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-30 19:22:20 +0900

    add CSVの書き込み

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from termcolor import colored
+import csv
+
 
 def robo_talk(str):
-  print(colored('🤖: ' + str + '\n', 'green'))
+    print(colored('🤖: ' + str + '\n', 'green'))
+
 
 def robo_question(str):
-  return input(colored('🤖: ' + str + '\n', 'green'))
+    return input(colored('🤖: ' + str + '\n', 'green'))
+
 
 your_name = robo_question('I am roboko. Please tell me your name?')
-favorite_restaurant = robo_question('What restaurant do you like?')
+favorite_restaurant = robo_question(
+    'Hi, ' + your_name + '. What restaurant do you like?')
+with open('list.csv', 'w') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'Name': favorite_restaurant, 'Count': 1})
 robo_talk('Thanks. Have a nice day.')
