74417c1870d812e7e42d66c3daebe412d0c38d57
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-01 20:24:44 +0900

    CSVの読み込み

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from termcolor import colored
 import csv
+import os
 
 
 def robo_talk(str):
@@ -10,12 +11,25 @@ def robo_question(str):
     return input(colored('🤖: ' + str + '\n', 'green'))
 
 
+csv_file = 'list.csv'
+
 your_name = robo_question('I am roboko. Please tell me your name?')
-favorite_restaurant = robo_question(
-    'Hi, ' + your_name + '. What restaurant do you like?')
-with open('list.csv', 'w') as csv_file:
-    fieldnames = ['Name', 'Count']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerow({'Name': favorite_restaurant, 'Count': 1})
-robo_talk('Thanks. Have a nice day.')
+
+if os.path.exists(csv_file):
+    dict = {}
+    with open(csv_file, newline='') as file:
+        reader = csv.reader(file)
+        next(file)
+        for row in reader:
+            dict[row[0]] = int(row[1])
+        print(dict)
+else:
+    favorite_restaurant = robo_question(
+        'Hi, ' + your_name + '. What restaurant do you like?')
+    favorite_restaurant = favorite_restaurant.title()
+    with open(csv_file, 'w') as csv_file:
+        fieldnames = ['Name', 'Count']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'Name': favorite_restaurant, 'Count': 1})
+    robo_talk('Thanks. Have a nice day.')
